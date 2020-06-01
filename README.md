70d329b97daec2a0d9f9ea383eb2d01d8898e823
Sreenivasulu Sreerama <ssreeram@centos76.mshome.net>
2020-06-01 12:34:39 -0700

    Script to prepare USB bootable disk with Ubuntu 20

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # usb-ubuntu20
-Prepare USB disk with Ubuntu20 image
+Prepare a bootable USB stick from ubuntu server 20 ISO image
+
+This script is used to prepare a USB stick with bootable
+Ubuntu 16.04 LTS image. The bootable disk is prepared from Ubuntu
+server install image. The host machine typically is a CentOS 7
+system and is tested with a CentOS 7.6 host.
+
+The script is not fully automated yet and the output is set to serial console. 
+
+The script is tested with Ubuntu server 20.04 version downloaded
+from:
+https://releases.ubuntu.com/20.04/ubuntu-20.04-live-server-amd64.iso
+
+This script can also be used to generate image suitable for running
+in the VM.
+
+GRUB installation:
+=================
+As part of installing Linux, GRUB will also be setup and the user will be prompted to
+select the disk to install. Don't select any drive and exit GRUB setup without
+installation. The GRUB bootloader will be setup by the script later. Picking up
+build system as destination may result in GRUB of the build system getting updated
+running the risk of the host system becoming non-bootable.
+
+Usage:
+=====
+Usage: ./ubuntu-iso-to-disk <path-to-ubuntu-iso> <target-device OR disk-image-filename>
+   target-device is argument of format /dev/sd*
+   disk-image-filename is meant to be consumed by a VM
+
---
+++ makeroot.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Adjust environment
+export PATH=$PATH:/sbin:/bin
+locale-gen "en_US.UTF-8"
+
+# Adjust sources list
+cat >/etc/apt/sources.list <<EOF
+deb file:/ubuntu focal main
+deb http://security.ubuntu.com/ubuntu focal main universe
+EOF
+
+# Perform update
+apt clean all
+apt update
+
+# Add packages
+apt install --assume-yes accountsservice/focal
+apt install --assume-yes build-essential/focal
+apt install --assume-yes dnsutils/focal
+apt install --assume-yes ssh/focal
+apt install --assume-yes telnet/focal
+apt install --assume-yes setserial/focal
+apt install --assume-yes lsof/focal
+apt install --assume-yes usbutils/focal
+apt install --assume-yes ethtool/focal
+apt install --assume-yes iptables/focal
+apt install --assume-yes ebtables/focal
+apt install --assume-yes gdisk/focal
+apt install --assume-yes dmidecode/focal
+apt install --assume-yes tcpdump/focal
+apt install --assume-yes python/focal python-pycurl/focal python-serial/focal
+apt install --assume-yes info/focal lshw/focal ntp/focal
+apt install --assume-yes zip/focal unzip/focal
+apt install --assume-yes software-properties-common/focal
+apt install --assume-yes libqmi-utils/focal modemmanager/focal
+sync
+
+echo ""
+echo "==========================================================="
+echo " Installing Linux now. Select no disk when prompted to     "
+echo " setup GRUB and exit without installing grub. The          "
+echo " bootloader is setup later. Picking up incorrect disk      "
+echo " may result in host system getting updated.                "
+echo "==========================================================="
+apt install --assume-yes linux-generic/focal
+sync
+
+# Install extra packages
+if [ -d /extrapkgs ]; then
+    apt install --assume-yes /extrapkgs/*.deb
+fi
+
+# Reset resolv.conf
+> /etc/resolv.conf
+
+# Redo GRUB configuration
+cat >/etc/default/grub <<EOF
+# Customized GRUB configuration 
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=2
+GRUB_TIMEOUT_STYLE="menu"
+GRUB_DISABLE_RECOVERY="true"
+GRUB_DISABLE_SUBMENU="true"
+GRUB_TERMINAL="serial"
+GRUB_SERIAL_COMMAND="serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1"
+GRUB_CMDLINE_LINUX_DEFAULT="quiet crashkernel=auto panic=2 net.ifnames=0 console=ttyS0,115200n8"
+EOF
+
+# Generate grub config now
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Open up 'root' account for login and SSH
+sed -i '/^root:/s/:x:/::/' /etc/passwd
+sed -i 's/^#\?PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/^#\?PermitEmptyPasswords .*/PermitEmptyPasswords yes/' /etc/ssh/sshd_config
+
+exit 0
